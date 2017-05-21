a59bb9eebe9d547c98aea723260c781cc5948d85
芊野 <free.nan.sky@gmail.com>
2017-05-21 22:25:25 +0800

    init

---
+++ examples/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <tool-tip></tool-tip>
+</template>
+
+<script>
+import ToolTip from '../src/index.vue'
+export default {
+  components: { ToolTip }
+}
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+  width: 100%;
+}
+
+</style>
---
+++ src/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>K O !</h1>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.component-tool-tip {
+  position: fiex;
+
+  * {
+    box-sizing: border-box;
+  }
+}
+</style>
